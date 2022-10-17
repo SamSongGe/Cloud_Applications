710e87bf5808f5bf5db88753e56a9e4e479855ae
Sam Song <94204299+SamSongGe@users.noreply.github.com>
2022-10-16 20:24:01 -0400

    Add files via upload

---
+++ Class Lab/Class Assignment 5/Sam_Song.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Filename: your_name.php
+Name: Ge (Sam) Song
+Description: Create a variable and save your name in it.
+Then display the variable and it should display the following:
+My name is your_name.
+
+*/
+//This is a comment.
+
+$name =  Sam_Song;
+echo "My name is ".$name;
+?>
---
+++ Class Lab/Class Assignment 5/even_numbers.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Filename: even_numbers.php
+Name:
+Description: This file does …
+*/
+for ($i = 0; $i <= 10; $i+=2) //seting varible i=0, until i=10, incrase 2 everytime.
+{
+  echo $i."<br>";
+}
+?>
