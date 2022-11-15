a189f30b1ee17d1a825eca180ddfb71600271bd5
Sam Song <94204299+SamSongGe@users.noreply.github.com>
2022-11-14 19:05:07 -0500

    Add files via upload

---
+++ Class Lab/ClassAssignment 11/Form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style media="screen">
+      .input {width: 230px}
+    </style>
+  </head>
+  <body>
+
+    <form class="" action="Capture.php" method="post">
+      <fieldset>
+        <label>LastName:</label>
+        <input type="text" name="LastName" class="input" placeholder="enter your LastName here"><br><br>
+        <label>FirstName:</label>
+        <input type="text" name="FistName" class="input" placeholder="enter your FirstName here"><br><br>
+        <label>GWID:</label>
+        <input type="text" name="GWID" class="input" placeholder="enter your GWID here"><br><br>
+        <input type="submit" value="Submit"> <input type="reset" value="Reset">
+      </fieldset>
+    </form>
+
+
+  </body>
+</html>
